b489f517b6550abbcec051fd460edb29b2c8d1a2
rigoigoexea <cristian_giner1@hotmail.com>
2016-02-14 23:45:33 +0100

    5. En la segunda activad podemos sumar los números y escribirlos con D.log

--- app/src/main/java/org/cgin/intentformath/SecondActivity.java
+++ app/src/main/java/org/cgin/intentformath/SecondActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -18,7 +19,34 @@ public class SecondActivity extends AppCompatActivity {
     }
 
     public void onCalculateButtonClicked (View view){
+
         //Debug register for the calculate button
         Log.d("estrella galicia", "caluclate button method in progress");
+
+        //Get the objects associated to the two EditText fields
+        EditText messageView = (EditText) findViewById(R.id.editText);
+        EditText message2View = (EditText) findViewById(R.id.editText2);
+
+        //Get the text from the editable text fields, using the previous objects
+        String firstStr = messageView.getText().toString();
+        String secondStr = message2View.getText().toString();
+
+        //Show the string procedent from the text fields in the debug console
+        Log.d("estrella galicia", "firstStr is " + firstStr);
+        Log.d("estrella galicia", "secondStr is " + secondStr);
+
+        //Convert the string into an int
+        int firstNum = Integer.parseInt(firstStr);
+        int secondNum = Integer.parseInt(secondStr);
+
+        //Make the operation with int
+        int calculatedNum = firstNum + secondNum;
+
+        //Convert the int into a string again to return it to the first activity
+        String calculatedStr = String.valueOf(calculatedNum);
+
+        //Show the calculated string on the debug console before send to the first activity
+        Log.d("estrella galicia", "The calculatedStr is " + calculatedStr);
+
     }
 }
