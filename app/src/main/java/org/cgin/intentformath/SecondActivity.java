deba81ca3f54fa44c440469678226773e241a807
rigoigoexea <cristian_giner1@hotmail.com>
2016-02-14 22:35:47 +0100

    3. Crear segunda actividad (layout, .java, AndroidManifest)

---
+++ app/src/main/java/org/cgin/intentformath/SecondActivity.java
@@ -0,0 +1,24 @@
+package org.cgin.intentformath;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+    }
+
+    public void onCalculateButtonClicked (View view){
+        //Debug register for the calculate button
+        Log.d("estrella galicia", "caluclate button method in progress");
+    }
+}
